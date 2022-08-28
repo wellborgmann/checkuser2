a3c964497a36ab9cc4bdd2acab1066540acb6466
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-28 12:30:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ apt-get install apache2 -y > /dev/null 2>&1
 apt-get install php5 libapache2-mod-php5 php5-mcrypt -y > /dev/null 2>&1
 service apache2 restart > /dev/null 2>&1
 apt-get install php5-curl > /dev/null 2>&1
+apt-get install php5 libapache2-mod-php5 php5-mcrypt -y > /dev/null 2>&1
 
 echo -ne "\033[1;32m INFORME A MESMA SENHA\033[1;37m: "; read senha
 cd ../var/www/html

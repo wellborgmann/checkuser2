6b6851706f9c4a0a609f968a53d79d60f3bfe79b
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-28 11:22:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # Created by: @DuTra01
 
 echo -ne "\033[1;32m INFORME A MESMA SENHA\033[1;37m: "; read senha
-//echo "<?php $pass=$senha;?>" > pass.php
+
 
 sed -i "s;1010;$senha;g" /var/www/html/pass.php > /dev/null 2>&1
 fi

5ebb501818b3e55713e9160a4d984c47c00a906c
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-28 11:40:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #   chmod +x install.sh
 #   ./install.sh
 
-# Created by: @DuTra01
+# Created by: @apollo404
 
 echo -ne "\033[1;32m INFORME A MESMA SENHA\033[1;37m: "; read senha
 cd ../var/www/html

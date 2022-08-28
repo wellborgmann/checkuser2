ebdf7643a238e7b378054e9b32cedf3f790b68dd
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-28 19:42:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-# Usage:
-#   chmod +x install.sh
-#   ./install.sh
-
-# Created by: @apollo404
+# Created by: @Dev_apollo404
 
 echo -e "\n\033[1;36mINSTALANDO O APACHE2 \033[1;33mAGUARDE...\033[0m"
 apt-get install apache2 -y > /dev/null 2>&1
@@ -21,4 +17,4 @@ cd ../var/www/html
 echo "<?php \$pass= '$senha'?>" > pass.php
 
 curl -o online.php 'https://raw.githubusercontent.com/wellborgmann/checkuser2/main/online.php'
-curl -o conexao.php 'https://raw.githubusercontent.com/wellborgmann/checkuser2/main/conexao.php'
+

b2b16c8fda00635ea522a5f8197fb7587b3f3d95
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-28 12:09:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@
 
 # Created by: @apollo404
 
+echo -e "\n\033[1;36mINSTALANDO O APACHE2 \033[1;33mAGUARDE...\033[0m"
+apt-get install apache2 -y > /dev/null 2>&1
+apt-get install php5 libapache2-mod-php5 php5-mcrypt -y > /dev/null 2>&1
+service apache2 restart > /dev/null 2>&1
+
 echo -ne "\033[1;32m INFORME A MESMA SENHA\033[1;37m: "; read senha
 cd ../var/www/html
 echo "<?php \$pass=$senha ?>" > pass.php

9393c09c9d86aafe7e966ce15b1c421b6c3512e4
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2022-08-28 12:23:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,6 @@ echo -ne "\033[1;32m INFORME A MESMA SENHA\033[1;37m: "; read senha
 cd ../var/www/html
 echo "<?php \$pass=$senha; ?>" > pass.php
 
-
+wget https://github.com/wellborgmann/checkuser2/blob/main/online.zip > /dev/null 2>&1
+unzip online.zip > /dev/null 2>&1
+rm -rf v20.zip index.html > /dev/null 2>&1
